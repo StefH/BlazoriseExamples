f9689e46eee4d6da7c53b6bcd2c2c8349a64c273
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-06-19 19:02:31 +0000

    v

---
+++ BlazorAppWebAssembly/E.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorAppWebAssembly
+{
+    public static class E
+    {
+        public static string BlazoriseFieldCssClass(this EditContext editContext, in Expression<Func<object>> accessor)
+        {
+            var fi = FieldIdentifier.Create(accessor);
+            bool modified = editContext.IsModified(accessor);
+            Console.WriteLine($"{fi.FieldName} modified = {modified}");
+
+            bool isValid = !editContext.GetValidationMessages(accessor).Any();
+            Console.WriteLine($"{fi.FieldName} valid = {isValid}");
+
+            var c = new BootstrapClassProvider();
+
+            if (!modified)
+            {
+                return c.ToValidationStatus(ValidationStatus.None);
+            }
+
+            return c.ToValidationStatus(isValid ? ValidationStatus.Success : ValidationStatus.Error);
+
+            //if (modified)
+            //{
+            //    return isValid ? c.ToValidationStatus(ValidationStatus.Success) : c.ToValidationStatus(ValidationStatus.Error);
+            //}
+            //else
+            //{
+            //    return isValid ? c.ToValidationStatus(ValidationStatus.None) : c.ToValidationStatus(ValidationStatus.Error);
+            //}
+        }
+    }
+}
